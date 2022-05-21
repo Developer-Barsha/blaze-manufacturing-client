81e12037f519bfcadd3aa21b9fa34158552e99a2
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-05-21 21:56:01 +0600

    nav, banner and some basics done

---
+++ src/Shared/Loader.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// const Loader = () => {
+//     return (
+//         <div className='flex justify-center h-screen items-center'>
+//             <img src="https://cdn.dribbble.com/users/1415337/screenshots/8053026/media/103f66972cf041738948be85dc16c2e7.gif" className='w-40' alt="" />
+//         </div>
+//     );
+// };
+ 
+// export default Loader;
